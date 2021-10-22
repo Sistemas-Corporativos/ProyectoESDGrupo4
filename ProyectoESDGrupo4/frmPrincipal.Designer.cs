32f38cbe9f991d2cf2f653061c3943aa18022458
Ronald Mojica <RONALDMO@TEXOPS.COM>
2021-10-22 12:46:07 -0600

    background image

--- ProyectoESDGrupo4/frmPrincipal.Designer.cs
+++ ProyectoESDGrupo4/frmPrincipal.Designer.cs
@@ -157,8 +157,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::ProyectoESDGrupo4.Properties.Resources.LOGO_Ministerio_de_HaciendaSV;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmPrincipal";
